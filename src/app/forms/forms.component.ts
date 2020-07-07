9f971763697467f1260dbb9c568c0ff230d3cb5c
unknown <kunalkeshri047@gmail.com>
2020-07-07 11:08:54 +0530

    Initial Commits

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,Validators, FormGroup } from '@angular/forms';
-import { Router } from '@angular/Router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
